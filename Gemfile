3e89431f7c38098580e3579ccb91c2319dd5cfa1
Steven Ko <chunyuko85@gmail.com>
2014-07-15 11:47:23 +0800

    web master

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development,:test do
   gem 'rspec-rails'
   gem 'spring'
   gem 'capistrano-rails'
+  gem 'capistrano'
 end
 
 # Use ActiveModel has_secure_password
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,6 +3,7 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://novelking.cc'
 SitemapGenerator::Sitemap.create do
-  add '/', :changefreq => 'weekly', :priority => 0.9
   add '/apply-writer', :changefreq => 'weekly', :priority => 0.9
 end
+
+SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
