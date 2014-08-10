b762bd57672137b1da2e07410b1555580f45af0a
Steven Ko <chunyuko85@gmail.com>
2014-08-10 11:37:38 +0800

    add ui folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'ui(/:action)', controller: 'ui'
+  
   root 'welcome#index'
   get 'apply-writer', to: 'welcome#apply'
   post 'apply-writer', to: 'welcome#apply_create'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,127 @@
 
 ActiveRecord::Schema.define(version: 20140714073539) do
 
+  create_table "admins", force: true do |t|
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "article_texts", force: true do |t|
+    t.text     "text",       limit: 16777215
+    t.integer  "article_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "article_texts", ["article_id"], name: "index_article_texts_on_article_id", using: :btree
+
+  create_table "articles", force: true do |t|
+    t.integer  "novel_id"
+    t.string   "link"
+    t.string   "title"
+    t.string   "subject"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "num",        default: 0
+    t.boolean  "is_show",    default: true
+  end
+
+  add_index "articles", ["link"], name: "index_articles_on_link", using: :btree
+  add_index "articles", ["novel_id"], name: "index_articles_on_novel_id", using: :btree
+  add_index "articles", ["num"], name: "index_articles_on_num", using: :btree
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "link"
+    t.string   "cat_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hot_ships", force: true do |t|
+    t.integer  "novel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "hot_ships", ["novel_id"], name: "index_hot_ships_on_novel_id", using: :btree
+
+  create_table "novels", force: true do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.string   "pic"
+    t.integer  "category_id"
+    t.string   "link"
+    t.string   "article_num"
+    t.string   "last_update"
+    t.boolean  "is_serializing"
+    t.boolean  "is_category_recommend"
+    t.boolean  "is_category_hot"
+    t.boolean  "is_category_this_week_hot"
+    t.boolean  "is_classic"
+    t.boolean  "is_classic_action"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.integer  "crawl_times",               default: 0
+    t.integer  "num",                       default: 0
+    t.boolean  "is_show",                   default: true
+  end
+
+  add_index "novels", ["author"], name: "index_novels_on_author", using: :btree
+  add_index "novels", ["category_id"], name: "index_novels_on_category_id", using: :btree
+  add_index "novels", ["is_show"], name: "index_novels_on_is_show", using: :btree
+  add_index "novels", ["name"], name: "index_novels_on_name", using: :btree
+  add_index "novels", ["num"], name: "index_novels_on_num", using: :btree
+
+  create_table "recommend_categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recommend_category_novel_ships", force: true do |t|
+    t.integer  "novel_id"
+    t.integer  "recommend_category_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  add_index "recommend_category_novel_ships", ["novel_id"], name: "index_recommend_category_novel_ships_on_novel_id", using: :btree
+  add_index "recommend_category_novel_ships", ["recommend_category_id"], name: "index_recommend_category_novel_ships_on_recommend_category_id", using: :btree
+
+  create_table "this_month_hot_ships", force: true do |t|
+    t.integer  "novel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "this_month_hot_ships", ["novel_id"], name: "index_this_month_hot_ships_on_novel_id", using: :btree
+
+  create_table "this_week_hot_ships", force: true do |t|
+    t.integer  "novel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "this_week_hot_ships", ["novel_id"], name: "index_this_week_hot_ships_on_novel_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "registration_id"
+    t.text     "read_novels"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "device_id"
+    t.text     "downloaded_novels"
+    t.text     "collected_novels"
+    t.string   "country"
+    t.string   "platform"
+    t.integer  "version"
+  end
+
+  add_index "users", ["device_id"], name: "index_users_on_device_id", using: :btree
+
   create_table "writers", force: true do |t|
     t.string   "name"
     t.string   "email"
