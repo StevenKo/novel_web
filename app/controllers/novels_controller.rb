e1b035e0544326285ada5eaae7942b123a599cdb
Steven Ko <chunyuko85@gmail.com>
2014-09-01 11:29:23 +0800

    novel & cateogry

---
+++ app/controllers/novels_controller.rb
@@ -0,0 +1,7 @@
+class NovelsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   
   root 'welcome#index'
+  get 'download', to: 'welcome#download'
   get 'apply-writer', to: 'welcome#apply'
   post 'apply-writer', to: 'welcome#apply_create'
+
+  resources :categories do
+    collection do 
+      get 'recommend'
+    end
+    resources :novels
+  end
+
 end
